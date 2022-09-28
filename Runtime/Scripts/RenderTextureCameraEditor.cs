60f8cb4367eb92e3eef7f896fefad938f693dec8
katsumasa <katsumasa@unity3d.com>
2022-09-28 10:25:38 +0900

    プロパティ名を変更しREADMEを加筆

--- Runtime/Scripts/RenderTextureCameraEditor.cs
+++ Runtime/Scripts/RenderTextureCameraEditor.cs
@@ -5,18 +5,39 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEditor;
 using System.IO;
-
+using System.Globalization;
 
 namespace UTJ.RenderTextureCamera
 {
     [CustomEditor(typeof(RenderTextureCamera))]
     public class RenderTextureCameraEditor : Editor
     {
+        static class Styles
+        {
+            
+        }
+
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
 
             var renderTextureCamera = target as RenderTextureCamera;
+
+            if(renderTextureCamera.renderTextureCamera == null)
+            {
+                EditorGUILayout.HelpBox("RenderTextureCameraが設定されていません。",MessageType.Error);
+            }
+            if(renderTextureCamera.blitCamera == null)
+            {
+                EditorGUILayout.HelpBox("BlitCameraが設定されていません。", MessageType.Error);
+            }
+            if(renderTextureCamera.renderTextureCamera != null && renderTextureCamera.blitCamera != null)
+            {
+                if((renderTextureCamera.renderTextureCamera.cullingMask & renderTextureCamera.blitCamera.cullingMask) != 0)
+                {
+                    EditorGUILayout.HelpBox($"{renderTextureCamera.blitCamera.name}と{renderTextureCamera.renderTextureCamera}のcullingMaskに一致するレイヤーが含まれています。レンダリング対象が重複しないようにcullingMaskの設定を見直して下さい。", MessageType.Error);
+                }
+            }
 
             EditorGUI.BeginChangeCheck();
             renderTextureCamera.shift = EditorGUILayout.IntSlider("Shit", renderTextureCamera.shift, 0, 6);
@@ -24,7 +45,7 @@ namespace UTJ.RenderTextureCamera
 
             if (EditorGUI.EndChangeCheck())
             {
-                renderTextureCamera.mIsDirty = true;
+                renderTextureCamera.m_IsDirty = true;
             }
         }
     }
